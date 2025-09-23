54d4c569495d58896d463d169058fd4d1ad22781
dreamer-1829 <nnodenexus@gmail.com>
2025-09-23 09:31:22 +0530

    enrollment data

---
+++ enrollment_database.sql
@@ -0,0 +1,3 @@
+INSERT INTO `student enrollment database`(`id`, `name`, `course`, `fees_paid`, `status`) VALUES (1, 'Alice', 'Web Development', 5000, 'Inactive'),
+(2, 'Bob', 'Data Science', 7000, 'Inactive'),
+(3, 'Charlie', 'UI/UX Design', 4000, 'Active')
---
+++ task01.sql
@@ -0,0 +1,3 @@
+INSERT INTO `students`(`book ID`, `Tittle`, `Author`, `Price`, `Stock`) VALUES (1, 'Learn SQL', 'John Smith', 400, 10),
+(2, 'Mastering Python', 'Jane Doe', 600, 5),
+(3, 'HTML & CSS Basics', 'Alan Webb', 300, 8)
