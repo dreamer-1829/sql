2e8b4efeea7d269482cd7203ba3fa503b93742db
dreamer-1829 <nnodenexus@gmail.com>
2025-09-23 09:45:15 +0530

    select and update

--- enrollment_database.sql
+++ enrollment_database.sql
@@ -1,3 +1,8 @@
 INSERT INTO `student enrollment database`(`id`, `name`, `course`, `fees_paid`, `status`) VALUES (1, 'Alice', 'Web Development', 5000, 'Inactive'),
 (2, 'Bob', 'Data Science', 7000, 'Inactive'),
 (3, 'Charlie', 'UI/UX Design', 4000, 'Active')
+
+SELECT * FROM `student enrollment database` WHERE fees_paid > 5000;
+
+UPDATE `student enrollment database` SET status='Active'
+WHERE course = 'Web Development';
